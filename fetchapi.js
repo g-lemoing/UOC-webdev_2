7cca315ae646d0733ae1a340ef77bf73ad93fef2
g-lemoing <glemoing@uoc.edu>
2022-07-29 19:03:20 +0200

    Load Index and Combat and change theme

---
+++ fetchapi.js
@@ -0,0 +1,183 @@
+loadCards();
+
+// Get an array of random numbers of Pokemons to be displayed
+function getRandomPokeNumbers(totalCards){
+    // Constant values
+    const MAX_POKE_NUMBER = 905;                            // Max Pokemon Number
+    console.log('Hello fetchapi')
+    // Variables
+    let pokenumbers = [];                // Array of selected Pokemons
+
+    // Build array of Pokemons 
+    while (pokenumbers.length < totalCards){
+        let pokenumber = (Math.floor(Math.random() * MAX_POKE_NUMBER)).toString();
+        console.log(pokenumber);
+        if(!pokenumbers.includes(pokenumber) && pokenumber > 0){
+            pokenumbers.push(pokenumber);
+        }
+    }
+    return pokenumbers;
+}
+
+// Get Pokemon data from fetch API
+function loadCards(){
+    const CARDS = 10;                                           // Number of cards to be loaded on page
+    const pokenumbers = getRandomPokeNumbers(CARDS);
+    const PATH_URL = "https://pokeapi.co/api/v2/pokemon/"       // Root URL path 
+    var pokeResult;                                             // FetchAPI result object
+
+    // Request from PokeAPI
+
+    pokenumbers.forEach(element => {
+        let fetchurl = PATH_URL + element;                      // Build url + id Pokemon
+        console.log(fetchurl);
+
+        fetch(fetchurl, createCard)                             // Fetch request
+        .then(response => response.json())
+        .then(data => pokeResult = {
+                "id": data.id,
+                "name": data.name,
+                "img": data.sprites.front_default,
+                "img-back": data.sprites.back_default,
+                "attack": data.stats[1].base_stat,
+                "defense": data.stats[2].base_stat,
+                "types": data.types
+        })
+        .then(() => createCard(pokeResult))
+    });
+}
+
+function createCard(cardObj) {
+    console.log(cardObj.img);
+    // Creem els objectes del grid i de la card
+    let cardGrid = document.getElementById('cards-grid');
+    let card = document.createElement('div');
+    card.setAttribute("id", "card");
+    // Creem els elements de cada card: imatge, nom, atac i defensa
+    let card_img = document.createElement('img');
+    card_img.src = cardObj.img;
+    card_img.alt = cardObj.name;
+    let card_name = document.createElement('h2');
+    card_name.innerHTML = (cardObj.name).charAt(0).toUpperCase()+cardObj.name.slice(1);
+    let card_attack = document.createElement('p');
+    card_attack.innerHTML = "Atac: " + cardObj.attack.toString();
+    let card_defense = document.createElement('p');
+    card_defense.innerHTML = "Defensa: " + cardObj.defense.toString();
+
+    // Afegim aquests 4 elements a la card
+    card.appendChild(card_img);
+    card.appendChild(card_name);
+    card.appendChild(card_attack);
+    card.appendChild(card_defense);
+
+    // Make card visibility hidden (card face down) if combat page
+    let pageName = location.pathname.split("/").slice(-1);
+    if (pageName == "combat.html"){
+      card.classList.add("card-down");
+    }
+
+    // Afegim la card al grid --> Hauria de ser al forEach de creació de la grid de cards
+    cardGrid.appendChild(card);
+}
+
+// Flip card up function
+function cardUp(card){
+  card.classList.add("card-up");
+  card.classList.remove("card-down");
+}
+
+// Fight winner function
+function card1Wins(card1, card2){
+  if (card1.attack > card2.defense){
+      alert(`${card1.name} ataca i guanya a ${card2.name}.`);
+  }
+  else{
+      alert(`${card2.name} ataca i guanya a ${card1.name}.`);
+  }
+}
+
+// Filter cards by name
+function filterCards() {
+    // Declare variables
+    let input, filter, cardsGrid, cards, h2, i, txtValue;
+    input = document.getElementById('name_search');
+    filter = input.value.toUpperCase();
+    cardsGrid = document.getElementById("cards-grid");
+    cards = cardsGrid.getElementsByTagName('div');
+  
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < cards.length; i++) {
+      h2 = cards[i].getElementsByTagName("h2")[0];
+      txtValue = h2.textContent || h2.innerText;
+      console.log(txtValue);
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        cards[i].style.display = "";
+      } else {
+        cards[i].style.display = "none";
+      }
+    }
+  }
+
+// Switch theme between light and dark
+function switchTheme(e){
+  console.log(e.target);
+  switch (e.target.value) {
+    case "dark":
+      body.setAttribute("data-theme", "dark");
+      console.log(typeof(nodes));
+      nodes.forEach((node) => {
+        node.classList.remove("icon");
+        node.classList.add("icon-dark-theme");
+      })
+      localStorage.setItem("customTheme", "dark");
+      break;
+    case "light":
+      body.setAttribute("data-theme", "light");
+      nodes.forEach((node) => {
+        node.classList.remove("icon-dark-theme");
+        node.classList.add("icon");
+      })
+      localStorage.setItem("customTheme", "light");
+    default:
+      body.setAttribute("data-theme", "light");
+      nodes.forEach((node) => {
+        node.classList.remove("icon-dark-theme");
+        node.classList.add("icon");
+      })
+      break;
+  }
+}
+
+  // Check custom theme in local storage
+  function checkTheme(){
+    let theme = localStorage.getItem("customTheme");
+    if (theme == null || theme != ''){
+      body.setAttribute("data-theme", theme);
+      document.getElementById(theme).checked = true;
+    }
+  }
+
+  // Event listeners
+  // Change theme listener
+  const body = document.getElementsByTagName('body')[0];
+  const switcher = document.querySelectorAll('input[name = "switcher"]');
+  body.setAttribute('data-theme', 'light');
+  switcher.forEach(
+    (sw) => {sw.addEventListener('change', switchTheme)
+  }
+  )
+  // Change fill color icons in nav
+  const element = document.getElementsByTagName("nav")[0];
+  const nodes = Array.from(element.getElementsByTagName("img"));
+
+  // Check theme when loading page
+  document.addEventListener("DOMContentLoaded", () => checkTheme());
+ 
+  // Event Click on card
+  const cards = document.querySelectorAll('cards-grid');
+  console.dir(cards);
+  cards.forEach((card) => {card.addEventListener('click', cardUp)});
+
+
+  // Desglosar creació de card (parametrizar si es de vista detalle o llistat?)
+  //--> afegir Event listener "click" --> afegir card a cards-grid
